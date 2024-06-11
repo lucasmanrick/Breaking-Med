f38da289d3691d1fb53b4c32dadaa541fc26d357
Lucas Fonseca <l0043995@ems.com.br>
2024-06-11 09:29:49 -0300

    excluindo arquivos iniciais desnecessários

--- src/controllers/consultaController.js
+++ src/controllers/consultaController.js
@@ -1,101 +1,102 @@
 const Pessoa = require('../models/Classes/PessoaClass');
 const Consulta = require('../models/Classes/consultaClass');
 const Prontuario = require('../models/Classes/prontuarioClass');
-const {registraNovaConsulta,retornaConsultaDePacienteLogado,retornaConsultaDeMedicoLogado,cancelaAgendamentoConsulta,retornaConsultasAdm,retornaEspecialidadesEFuncionarioVinculado,updateDadosProntuário} = require('../models/Queries/ConsultaQuerie');
-const {verificaExistenciaDeUmUsuario} = require('../models/Queries/PessoaQuerie');
+const { registraNovaConsulta, retornaConsultaDePacienteLogado, retornaConsultaDeMedicoLogado, cancelaAgendamentoConsulta, retornaConsultasAdm, retornaEspecialidadesEFuncionarioVinculado, updateDadosProntuário } = require('../models/Queries/ConsultaQuerie');
+const { verificaExistenciaDeUmUsuario } = require('../models/Queries/PessoaQuerie');
 
 
 const consultaController = {
 
-  retornaEspecialidadesEFuncionario: async (req,res) => {
-    const {funcionarioId} = req.params
+  retornaEspecialidadesEFuncionario: async (req, res) => {
+    const { funcionarioId } = req.params
     let result;
-    if(funcionarioId) {
-       result = await retornaEspecialidadesEFuncionarioVinculado(funcionarioId)
+    if (funcionarioId) {
+      result = await retornaEspecialidadesEFuncionarioVinculado(funcionarioId)
       res.json(result)
-    }else {
-       result = await retornaEspecialidadesEFuncionarioVinculado()
+    } else {
+      result = await retornaEspecialidadesEFuncionarioVinculado()
       res.json(result)
     }
   },
-  retornaTodasConsultas: async (req,res) => {
+  retornaTodasConsultas: async (req, res) => {
     try {
       const retornoConsulta = await retornaConsultasAdm()
       res.json(retornoConsulta)
     }
-    catch(e) {
+    catch (e) {
       console.log(e)
-      res.json({consultaMessage:'não foi possivel retornar consultas ao adm, por favor tente novamente', result:false})
+      res.json({ consultaMessage: 'não foi possivel retornar consultas ao adm, por favor tente novamente', result: false })
     }
   },
 
-  novaConsulta: async (req,res) => {
+  novaConsulta: async (req, res) => {
     try {
-      const {data,hora,status,cpfPaciente,funcionario_id,funcionario_pessoa_id,especialidade_id} = req.body;
-      
+      const { data, hora, status, cpfPaciente, funcionario_id, funcionario_pessoa_id, especialidade_id } = req.body;
+
       const verificaExistenciaPaciente = await verificaExistenciaDeUmUsuario(cpfPaciente)
       let consultaObj;
 
-      
+
       console.log(verificaExistenciaPaciente)
-      if(verificaExistenciaPaciente.result === true) {
-        consultaObj = new Consulta(null,data,hora,status,verificaExistenciaPaciente.moreInfos.pac_id,verificaExistenciaPaciente.moreInfos.id ,funcionario_id,funcionario_pessoa_id,especialidade_id)
-      }else {
+      if (verificaExistenciaPaciente.result === true) {
+        consultaObj = new Consulta(null, data, hora, status, verificaExistenciaPaciente.moreInfos.pac_id, verificaExistenciaPaciente.moreInfos.id, funcionario_id, funcionario_pessoa_id, especialidade_id)
+      } else {
         res.json(verificaExistenciaPaciente)
         return
       }
 
-     
-     res.json(await registraNovaConsulta(consultaObj))
+
+      res.json(await registraNovaConsulta(consultaObj))
     }
     catch (e) {
-      res.json({consultaMessage: 'não foi possivel registrar consulta tente novamente.', result:false})
+      res.json({ consultaMessage: 'não foi possivel registrar consulta tente novamente.', result: false })
       console.log(e)
     }
   },
 
-  verificaConsultasPaciente: async (req,res) => {
+  verificaConsultasPaciente: async (req, res) => {
     const idPessoa = req.headers['idpessoa']  //o id recebido aki é o do paciente
-    if(idPessoa) {
-      const recebeConsultas = new Pessoa (idPessoa)
-      res.json(await retornaConsultaDePacienteLogado (recebeConsultas))
+    if (idPessoa) {
+      const recebeConsultas = new Pessoa(idPessoa)
+      res.json(await retornaConsultaDePacienteLogado(recebeConsultas))
       return
     } else {
-      res.json({consultaMessage:'o id do paciente que está solicitando consultas pendentes não foi passado', result:false})
+      res.json({ consultaMessage: 'o id do paciente que está solicitando consultas pendentes não foi passado', result: false })
     }
   },
 
-  verificaConsultasMedico:async (req,res) => {
+  verificaConsultasMedico: async (req, res) => {
     const idFuncionario = req.headers['idFuncionario']
-    if(idFuncionario) {
-      const recebeConsultas = new Pessoa (idFuncionario)
-      res.json(await retornaConsultaDeMedicoLogado (recebeConsultas))
+    if (idFuncionario) {
+      const recebeConsultas = new Pessoa(idFuncionario)
+      res.json(await retornaConsultaDeMedicoLogado(recebeConsultas))
     }
   },
 
-  cancelaConsulta: async (req,res) => {
+  cancelaConsulta: async (req, res) => {
     try {
       const idDaConsulta = req.headers['idconsulta'];
       console.log(req.headers)
-      if(!idDaConsulta) {
-        res.json({consultaMessage:'o id da consulta não chegou corretamente ao servidor, tente novamente!',result:false})
+      if (!idDaConsulta) {
+        res.json({ consultaMessage: 'o id da consulta não chegou corretamente ao servidor, tente novamente!', result: false })
         return
-      } 
+      }
+
+      res.json(await cancelaAgendamentoConsulta(idDaConsulta))
     } catch (e) {
       console.log(e)
     }
-    
 
-   res.json(await cancelaAgendamentoConsulta(idDaConsulta))
+
   },
 
-  preencheProntuario: async (req,res) => {
+  preencheProntuario: async (req, res) => {
     try {
-      const {diagnostico,medicacao} = req.body;
-      const {idPront} = req.body;
-  
-      const instanciaPront = new Prontuario(idPront,diagnostico,medicacao)
-  
+      const { diagnostico, medicacao } = req.body;
+      const { idPront } = req.body;
+
+      const instanciaPront = new Prontuario(idPront, diagnostico, medicacao)
+
       const sendReceiveUpdate = updateDadosProntuário(instanciaPront)
     } catch (e) {
       console.log(e)
